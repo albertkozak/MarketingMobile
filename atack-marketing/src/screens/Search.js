7574da0c5df846515fc592ca3027db15999e24fd
Albert Kozak <albert.kozak@live.com>
2020-04-16 19:29:15 -0700

    Added search bar component + linked to search screen

--- atack-marketing/src/screens/Search.js
+++ atack-marketing/src/screens/Search.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import { SearchBar } from "../components/SearchBar";
 
-const Search = ({ navigation }) => {
+const SearchScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {/* <SearchBar /> */}
       <Text>Search Screen</Text>
     </View>
   );
 };
 
-export default Search;
+export default SearchScreen;
