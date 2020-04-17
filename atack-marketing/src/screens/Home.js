0bf177bdd4a2a87ecf4996ee851e9289576c934f
Albert Kozak <albert.kozak@live.com>
2020-04-16 18:01:39 -0700

    Updated base code home screen

--- atack-marketing/src/screens/Home.js
+++ atack-marketing/src/screens/Home.js
@@ -1,11 +1,10 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
-      <Button title="Search" onPress={() => navigation.navigate("Details")} />
     </View>
   );
 }
