478e0b644b75c623c4bdaff6e64a03f1da629fea
Athena <athenakozak@gmail.com>
2020-04-17 15:01:37 -0700

    Added VendorList + VendorItem components

--- atack-marketing/src/components/vendors/VendorList.js
+++ atack-marketing/src/components/vendors/VendorList.js
@@ -1,18 +1,43 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import Vendor from './Vendor'
+import { TouchableOpacity } from "react-native-gesture-handler";
+import VendorItem from './VendorItem'
 
-const VendorList = ({ navigation, props }) => {
+const VendorList = ({ navigation }) => {
+  const dummyData = [
+    {eventTitle: "Vancouver Tech Conferencee 2020", vendorName: "Amazon", marketMaterials: ['Coffee Mugs', 'Mouse Pads', 'Keychains']}
+  ]
+
+  const showVendorDetail = (vendor) => {
+    navigation.navigate('Vendor', vendor)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Vendor List</Text>
+      <Text style={styles.title}>Vendors</Text>
+      <FlatList 
+        keyExtractor={vendor => vendor.vendorName}
+        data={dummyData}
+        renderItem={({item}) => {
+          return <TouchableOpacity onPress={() => showVendorDetail(item)}>
+            <VendorItem vendor={item} />
+          </TouchableOpacity>
+        }}
+      >
+        <Vendor />
+      </FlatList>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
    flex: 1, 
    alignItems: "center", 
    justifyContent: "center" 
+  }
 })
 
 export default VendorList;
