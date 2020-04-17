3f8afb03cfed9b5e00604f91a0c740dac3918e36
Athena <athenakozak@gmail.com>
2020-04-17 14:34:22 -0700

    Added vendor detail placeholder elements

--- atack-marketing/src/components/vendors/Vendor.js
+++ atack-marketing/src/components/vendors/Vendor.js
@@ -1,13 +1,26 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, FlatList} from "react-native";
 
-function Vendor({ navigation }) {
+const Vendor = ({ props }) => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Vendor Screen</Text>
+    <View style={styles.container}>
+      {/* Update with dynamic info */}
+      <Text stlye={styles.eventTitle}>Vancouver Tech Conference 2020</Text>
+      <Text>Amazon</Text>
+      <FlatList style={styles.list}>
+        <Text style={styles.listItem}>Coffee Mugs</Text>
+        <Text style={styles.listItem}>Mouse Pads</Text>
+        <Text style={styles.listItem}>Keychain</Text>
+      </FlatList>
       <Button title="Subscribe" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+   flex: 1, 
+   alignItems: "center", 
+   justifyContent: "center" 
+})
 
 export default Vendor;
--- atack-marketing/src/components/vendors/VendorList.js
+++ atack-marketing/src/components/vendors/VendorList.js
@@ -1,12 +1,18 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-function VendorList({ navigation }) {
+const VendorList = ({ navigation, props }) => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.container}>
       <Text>Vendor List</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+   flex: 1, 
+   alignItems: "center", 
+   justifyContent: "center" 
+})
 
 export default VendorList;
