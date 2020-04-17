d6e60456adb3cb5fdc9df60a5ef0756d8fbdb42b
Albert Kozak <albert.kozak@live.com>
2020-04-16 18:00:06 -0700

    Updated base code on search screen

--- atack-marketing/src/screens/Search.js
+++ atack-marketing/src/screens/Search.js
@@ -1,14 +1,10 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
 
 const Search = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push("Details")}
-      />
+      <Text>Search Screen</Text>
     </View>
   );
 };
