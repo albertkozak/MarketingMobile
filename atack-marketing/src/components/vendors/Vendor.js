458779c2d59ef30ae1f70c6232c5e9c580040e59
Athena <athenakozak@gmail.com>
2020-04-17 15:02:04 -0700

    Refactored Vendor Code to be dynamic

--- atack-marketing/src/components/vendors/Vendor.js
+++ atack-marketing/src/components/vendors/Vendor.js
@@ -1,16 +1,19 @@
 import * as React from "react";
-import { View, Text, Button, FlatList} from "react-native";
+import { View, Text, Button, FlatList, StyleSheet} from "react-native";
 
-const Vendor = ({ props }) => {
+const Vendor = ({ route }) => {
+  const { eventTitle, vendorName, marketMaterials } = route.params 
   return (
     <View style={styles.container}>
       {/* Update with dynamic info */}
-      <Text stlye={styles.eventTitle}>Vancouver Tech Conference 2020</Text>
-      <Text>Amazon</Text>
-      <FlatList style={styles.list}>
-        <Text style={styles.listItem}>Coffee Mugs</Text>
-        <Text style={styles.listItem}>Mouse Pads</Text>
-        <Text style={styles.listItem}>Keychain</Text>
+  <Text stlye={styles.eventTitle}>{eventTitle}</Text>
+  <Text>{vendorName}</Text>
+      <FlatList>
+          data={marketMaterials}
+          keyExtractor={item => item}
+          renderItem{({item}) => {
+            return <Text style={styles.listItem}>{item}</Text>
+          }}
       </FlatList>
       <Button title="Subscribe" />
     </View>
@@ -18,9 +21,11 @@ const Vendor = ({ props }) => {
 }
 
 const styles = StyleSheet.create({
+  container: {
    flex: 1, 
    alignItems: "center", 
    justifyContent: "center" 
+  }
 })
 
 export default Vendor;
