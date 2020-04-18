a6407c3025d703241b44706f3ed5a8c4386e329b
Albert Kozak <albert.kozak@live.com>
2020-04-17 17:33:14 -0700

    Added placeholder elements to EventList

--- atack-marketing/src/components/events/EventList.js
+++ atack-marketing/src/components/events/EventList.js
@@ -1,12 +1,44 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-function EventList({ navigation }) {
+const EventList = ({ navigation }) => {
+  const dummyData = [
+    {
+      eventTitle: "Vancouver Tech Conferencee 2020",
+    },
+  ];
+
+  const showEventDetail = (event) => {
+    navigation.navigate("Event", event);
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Event List</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Events</Text>
+      <FlatList
+        keyExtractor={(event) => event.eventTitle}
+        data={dummyData}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() => showEventDetail(item)}>
+              <EventItem event={item} />
+            </TouchableOpacity>
+          );
+        }}
+      >
+        <Event />
+      </FlatList>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default EventList;
