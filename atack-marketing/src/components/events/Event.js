c9d8692b36299a4c478d8d8a07ca283171d079e3
Albert Kozak <albert.kozak@live.com>
2020-04-19 13:18:59 -0700

    Refactored event code to pass dynamic values

--- atack-marketing/src/components/events/Event.js
+++ atack-marketing/src/components/events/Event.js
@@ -3,15 +3,17 @@ import { View, Text, Button, StyleSheet } from "react-native";
 
 const Event = ({ route, navigation }) => {
   const { eventTitle } = route.params;
+
+  const showVendorList = (vendorList) => {
+    navigation.navigate("VendorList", vendorList);
+  };
+
   return (
     <View style={styles.container}>
       {/* Update with dynamic info */}
       <Text stlye={styles.eventTitle}>{eventTitle}</Text>
       <Button title="Join" onPress={() => navigation.navigate("QRScan")} />
-      <Button
-        title="Vendors"
-        onPress={() => navigation.navigate("VendorList")}
-      />
+      <Button title="Vendors" onPress={() => showVendorList(eventTitle)} />
     </View>
   );
 };
--- atack-marketing/src/components/vendors/VendorList.js
+++ atack-marketing/src/components/vendors/VendorList.js
@@ -1,42 +1,47 @@
 import * as React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
-import Vendor from './Vendor'
+import Vendor from "./Vendor";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import VendorItem from './VendorItem'
+import VendorItem from "./VendorItem";
 
-const VendorList = ({ navigation }) => {
+const VendorList = ({ navigation, route }) => {
+  const { eventTitle } = route.params;
   const dummyData = [
-    {eventTitle: "Vancouver Tech Conferencee 2020", vendorName: "Amazon", marketMaterials: ['Coffee Mugs', 'Mouse Pads', 'Keychains']}
-  ]
+    {
+      eventTitle: "Vancouver Tech Conferencee 2020",
+      vendorName: "Amazon",
+      marketMaterials: ["Coffee Mugs", "Mouse Pads", "Keychains"],
+    },
+  ];
 
   const showVendorDetail = (vendor) => {
-    navigation.navigate('Vendor', vendor)
-  }
+    navigation.navigate("Vendor", vendor);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Vendors</Text>
-      <FlatList 
-        keyExtractor={vendor => vendor.vendorName}
+      <FlatList
+        keyExtractor={(vendor) => vendor.vendorName}
         data={dummyData}
-        renderItem={({item}) => {
-          return <TouchableOpacity onPress={() => showVendorDetail(item)}>
-            <VendorItem vendor={item} />
-          </TouchableOpacity>
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() => showVendorDetail(item)}>
+              <VendorItem vendor={item} />
+            </TouchableOpacity>
+          );
         }}
-      >
-      </FlatList>
-
+      ></FlatList>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1, 
-   alignItems: "center", 
-   justifyContent: "center" 
-  }
-})
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default VendorList;
