a632a423ace33669e536ff6d5ede4263ec301c55
Albert Kozak <albert.kozak@live.com>
2020-04-18 20:24:22 -0700

    Created Global Style Container

---
+++ atack-marketing/src/components/Container.js
@@ -0,0 +1,28 @@
+import React from "react";
+import LinearGradient from "react-native-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+
+// Linear Gradient Background
+const Container = ({ children }) => {
+  return (
+    <LinearGradient
+      colors={["#1C2233", "#363844", "#0C0C0C"]}
+      style={styles.gradient}
+    >
+      <SafeAreaView style={styles.container}>{children}</SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    marginHorizontal: 15,
+  },
+});
+
+export default Container;
