54313da6cf9c4e7ba2cb3c06cc8d53c43ff592c5
Athena <athenakozak@gmail.com>
2020-04-21 13:49:08 -0700

    added icons to event

--- atack-marketing/src/components/events/Event.js
+++ atack-marketing/src/components/events/Event.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
 import Container from "../Container";
 import Colors from "../../constants/Color";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 const Event = ({ route, navigation }) => {
   const { eventName, eventStartDateTime, numOfVendors, venue } = route.params;
@@ -15,8 +16,14 @@ const Event = ({ route, navigation }) => {
       <SafeAreaView style={styles.wrapper}>
         {/* Update with dynamic info */}
         <Text style={styles.eventTitle}>{eventName}</Text>
+        <View style={styles.location}>
+        <MaterialIcons name="location-on" size={18} color={Colors.GREY} />
         <Text style={styles.eventVenue}>{venue.venueName}</Text>
+        </View>
+        <View style={styles.start}>
+        <Ionicons name="ios-time" size={18} color={Colors.GREY} />
         <Text style={styles.eventStart}>{eventStartDateTime}</Text>
+        </View>
         <Text style={styles.eventVendors}>Vendors: {numOfVendors}</Text>
         <View style={styles.buttonContainer}>
           <Button
@@ -46,10 +53,19 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 25,
   },
+  location: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  start: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
   eventVenue: {
     color: Colors.WHITE,
     marginBottom: 25,
     fontSize: 18,
+    marginLeft: 10
   },
   eventVendors: {
     color: Colors.GREY,
@@ -60,6 +76,7 @@ const styles = StyleSheet.create({
     color: Colors.WHITE,
     marginBottom: 25,
     fontSize: 15,
+    marginLeft: 10
   },
   buttonContainer: {
     alignSelf: 'center',
