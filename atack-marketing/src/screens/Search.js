9b963f2407dd949ec46679aec3a7d3302fc6e683
Albert Kozak <albert.kozak@live.com>
2020-04-21 20:14:51 -0700

    Added search bar to search screen + connected to API

--- atack-marketing/src/screens/Search.js
+++ atack-marketing/src/screens/Search.js
@@ -1,20 +1,70 @@
-import * as React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { SearchBar } from "../components/SearchBar";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import SearchBar from "../components/SearchBar";
+import EventItem from "../components/events/EventItem";
 import Container from "../components/Container";
 import Colors from "../constants/Color";
+import firebase from "../firebase";
 
 const SearchScreen = ({ navigation }) => {
+  const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/Events";
+  const [term, setTerm] = useState("");
+  const [events, setEvents] = useState([]);
+
+  const searchAPI = () => {
+    firebase
+      .auth()
+      .currentUser.getIdTokenResult()
+      .then((tokenResponse) => {
+        fetch(BASE_URL, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${tokenResponse.token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((responseData) => {
+            setEvents(responseData.events);
+            console.log(events);
+          });
+      });
+  };
+
+  useEffect(() => {
+    searchAPI();
+  }, []);
+
+  const showEventDetail = (event) => {
+    navigation.navigate("Event", event);
+  };
+
   return (
     <Container>
       <View style={styles.wrapper}>
-        {/* <SearchBar /> */}
-        <Text style={styles.title}>Search Screen</Text>
-        <Button
-          title="Go to Events"
-          color={Colors.ORANGE}
-          onPress={() => navigation.navigate("EventList")}
+        <SearchBar
+          term={term}
+          onTermChange={(newTerm) => setTerm(newTerm)}
+          onTermSubmit={() => searchAPI()}
         />
+        <FlatList
+          keyExtractor={(event) => event.eventId.toString()}
+          data={events}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity onPress={() => showEventDetail(item)}>
+                <EventItem event={item} />
+              </TouchableOpacity>
+            );
+          }}
+        ></FlatList>
       </View>
     </Container>
   );
