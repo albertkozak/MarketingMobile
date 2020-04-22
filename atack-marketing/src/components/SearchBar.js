83fbc856445c37ba1b0e3a5c021690892ba1a833
Albert Kozak <albert.kozak@live.com>
2020-04-21 19:58:39 -0700

    Created search bar component

--- atack-marketing/src/components/SearchBar.js
+++ atack-marketing/src/components/SearchBar.js
@@ -1,12 +1,36 @@
 import * as React from "react";
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <TextInput placeholder="search" />
+    <View style={styles.background}>
+      <TextInput
+        placeholder="search"
+        value={term}
+        style={styles.input}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+        onSubmitEditing={() => onTermSubmit()}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    height: 50,
+    borderRadius: 6,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+  },
+  icon: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
 
 export default SearchBar;
