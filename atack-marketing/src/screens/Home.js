6473f009229528ff434d1d5ceea295215b3e6a59
Kasra <kniktash@gmail.com>
2020-04-22 14:46:31 -0700

    Home page with list of events added

--- atack-marketing/src/screens/Home.js
+++ atack-marketing/src/screens/Home.js
@@ -1,22 +1,63 @@
-import * as React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, StyleSheet, FlatList, SafeAreaView } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import EventItem from "../components/events/EventItem";
 import Container from "../components/Container";
 import Colors from "../constants/Color";
+import firebase from "../firebase";
 
-function HomeScreen({ navigation }) {
+
+const HomeScreen = ({ navigation }) => {
+  const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/Events";
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const fetchData = () => {
+    firebase
+      .auth()
+      .currentUser.getIdTokenResult()
+      .then((tokenResponse) => {
+        fetch(BASE_URL, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${tokenResponse.token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((responseData) => {
+            setFetchedData(responseData.events);
+            console.log(fetchedData);
+          });
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const showEventDetail = (event) => {
+    navigation.navigate("Event", event);
+  };
+
   return (
     <Container>
-      <View style={styles.wrapper}>
-        <Text style={styles.title}>Home Screen</Text>
-        <Button
-          title="Go to Events"
-          color={Colors.ORANGE}
-          onPress={() => navigation.navigate("EventList")}
-        />
-      </View>
+      <SafeAreaView style={styles.wrapper}>
+        <Text style={styles.title}>Home Page</Text>
+        <FlatList
+          keyExtractor={(event) => event.eventId.toString()}
+          data={fetchedData}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity onPress={() => showEventDetail(item)}>
+                <EventItem event={item} />
+              </TouchableOpacity>
+            );
+          }}
+        ></FlatList>
+      </SafeAreaView>
     </Container>
   );
-}
+};
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -27,8 +68,10 @@ const styles = StyleSheet.create({
   title: {
     color: Colors.WHITE,
     fontSize: 22,
+    textTransform: "uppercase",
     marginBottom: 25,
   },
 });
 
 export default HomeScreen;
+
