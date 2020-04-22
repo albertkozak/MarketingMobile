93c54b3d6c6c5d5c88ab8925e22354b5ac662393
Albert Kozak <albert.kozak@live.com>
2020-04-21 20:31:45 -0700

    Apply base styling to search bar

--- atack-marketing/src/components/SearchBar.js
+++ atack-marketing/src/components/SearchBar.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
+import Colors from "../constants/Color";
+import { colors } from "react-native-elements";
 
 const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.background}>
       <TextInput
-        placeholder="search"
+        placeholder="Search Events"
+        placeholderTextColor={Colors.GREY}
         value={term}
         style={styles.input}
         onChangeText={(newTerm) => onTermChange(newTerm)}
@@ -18,13 +21,17 @@ const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
 const styles = StyleSheet.create({
   background: {
     height: 50,
-    borderRadius: 6,
-    marginHorizontal: 15,
+    borderRadius: 10,
     flexDirection: "row",
     alignItems: "center",
+    borderColor: Colors.GREY,
+    borderWidth: 1,
+    paddingHorizontal: 15,
   },
   input: {
     flex: 1,
+    color: Colors.WHITE,
+    fontSize: 15,
   },
   icon: {
     fontSize: 35,
