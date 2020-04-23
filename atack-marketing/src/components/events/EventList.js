fff9774494fd06e1cbf69d4f371c24d6b300d357
Albert Kozak <albert.kozak@live.com>
2020-04-23 14:58:56 -0700

    Added filtered search functionality

--- atack-marketing/src/components/events/EventList.js
+++ atack-marketing/src/components/events/EventList.js
@@ -6,32 +6,32 @@ import Container from "../Container";
 import Colors from "../../constants/Color";
 import firebase from "../../firebase";
 
-const EventList = ({ navigation }) => {
-  const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/Events";
-  const [fetchedData, setFetchedData] = useState([]);
+const EventList = ({ navigation, results }) => {
+  // const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/Events";
+  // const [fetchedData, setFetchedData] = useState([]);
 
-  const fetchData = () => {
-    firebase
-      .auth()
-      .currentUser.getIdTokenResult()
-      .then((tokenResponse) => {
-        fetch(BASE_URL, {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${tokenResponse.token}`,
-          },
-        })
-          .then((response) => response.json())
-          .then((responseData) => {
-            setFetchedData(responseData.events);
-          });
-      });
-  };
+  // const fetchData = () => {
+  //   firebase
+  //     .auth()
+  //     .currentUser.getIdTokenResult()
+  //     .then((tokenResponse) => {
+  //       fetch(BASE_URL, {
+  //         method: "GET",
+  //         headers: {
+  //           Accept: "application/json",
+  //           Authorization: `Bearer ${tokenResponse.token}`,
+  //         },
+  //       })
+  //         .then((response) => response.json())
+  //         .then((responseData) => {
+  //           setFetchedData(responseData.events);
+  //         });
+  //     });
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   const showEventDetail = (event) => {
     navigation.navigate("Event", event);
@@ -43,7 +43,7 @@ const EventList = ({ navigation }) => {
         <Text style={styles.title}>Events</Text>
         <FlatList
           keyExtractor={(event) => event.eventId.toString()}
-          data={fetchedData}
+          data={results}
           renderItem={({ item }) => {
             return (
               <TouchableOpacity onPress={() => showEventDetail(item)}>
